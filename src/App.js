eb6d54da83920f86f6bc21513ef96030f13f71fe
unknown <aathavan.selvam@rhibhus.com>
2025-03-27 19:34:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import AddLoan from "./components/addLoan/Addloans.js";
 import Loanstable from "./components/tableLoans/Loanstable.js";
 import Profile from "./components/profile/Profile.js";
 import History from "./components/histories/History.js";
+
 const App = () => {
   return (
-    <Router> {/* ✅ Wrap the whole Routes inside BrowserRouter */}
+    <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/addloans" element={<AddLoan />} />
         <Route path="/allLoans" element={<Loanstable />} />
         <Route path="/histories" element={<History />} />
+        {/* Fallback route to handle undefined paths */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
